6cccfd0275c82e7501961279e76ed1ba90c45613
Jaesung Park <richdad6208@gmail.com>
2024-01-12 13:22:48 +0900

    design: 홈 화면 구현중

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,17 @@
+import style from "./page.module.css";
+
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <section className={style.content}>
+      <header>
+        <h2>홈</h2>
+        <div className={style.tabMenu}>
+          <ul>
+            <li>추천</li>
+            <li>팔로우 중</li>
+          </ul>
+        </div>
+      </header>
+    </section>
+  );
 }
